b1573288e86185ed96dd031ad4b63aa69d328504
Rudra Gowda H <121507406+Rudragowdah@users.noreply.github.com>
2024-11-10 11:35:09 +0530

    Login And SignUp Form

--- client/login.php
+++ client/login.php
@@ -1,24 +1,16 @@
 <div class="container">
-    <h2 class="heading">Sign Up</h2>
+    <h2 class="heading">Login</h2>
 <form >
     <div class="col-6 offset-sm-3 margin-bottom-15">
         <label for="username" class="form-label">Username</label>
         <input type="text" class="form-control" id="username" placeholder="Enter Your Username...">
     </div>
     <div class="col-6 offset-sm-3 margin-bottom-15">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter Your Email...">
-    </div>
-    <div class="col-6 offset-sm-3 margin-bottom-15">
         <label for="password" class="form-label">Password</label>
         <input type="password" class="form-control" id="password" placeholder="Enter Your password...">
     </div>
     <div class="col-6 offset-sm-3 margin-bottom-15">
-        <label for="address" class="form-label">Address</label>
-        <input type="password" class="form-control" id="address" placeholder="Enter Your Address...">
-    </div>
-    <div class="col-6 offset-sm-3 margin-bottom-15">
-        <button type="submit" class="btn btn-primary">Sign Up</button>
+        <button type="submit" class="btn btn-primary">Login</button>
     </div>
 </form>
 </div>
--- index.php
+++ index.php
@@ -4,11 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./public/style.css">
     <?php include('./client/commonFiles.php'); ?>
 </head>
 <body>
-    <?php include('./client/header.php'); ?>
-    <?php include('./client/signup.php'); ?>
+    <?php include('./client/header.php'); 
+    if(isset($_GET["signup"])) {
+        include('./client/signup.php');
+    }
+    elseif(isset($_GET['login'])) {
+        include("./client/login.php");
+    }
+    else {
+        // Something Else
+    }
+    ?>
 </body>
 </html>
